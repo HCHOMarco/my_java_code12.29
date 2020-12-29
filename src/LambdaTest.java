f8b9bf33de8cd8a86955ce6a2a6c653f11798491
HCHO <2434721018@qq.com>
2020-12-29 16:19:39 +0800

    Initial commit

---
+++ src/LambdaTest.java
@@ -0,0 +1,60 @@
+/**
+ * @author marco
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        LambdaTest tester = new LambdaTest();
+
+        // 声明类型
+        MathOperation addition = (int a, int b) -> a + b;
+
+        // 不用类型声明
+        MathOperation subtraction = (a, b) -> a - b;
+
+        // 大括号中的返回语句
+        MathOperation multiplication = (int a, int b) -> {
+            return a * b;
+        };
+
+        // 没有大括号及返回值
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operator(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operator(10, 5, subtraction));
+        System.out.println("10 * 5 = " + tester.operator(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operator(10, 5, division));
+
+        // 不用括号
+        GreetingService greetingService1 = message ->
+                System.out.println("Hello " + message);
+
+        // 用括号
+        GreetingService greetingService2 = message ->
+                System.out.println("Hello " + message);
+
+        greetingService1.sayMessage("Runoob");
+        greetingService2.sayMessage("Google");
+    }
+
+    /**
+     *
+     */
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    /**
+     *
+     */
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    /**
+     *
+     */
+    private int operator(int a, int b, MathOperation mathOperation) {
+        return mathOperation.operation(a, b);
+    }
+}
